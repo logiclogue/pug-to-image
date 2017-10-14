97f05cce02c7de0caee4a62fd1ef783db1917b2e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-10-14 19:35:57 +0100

    Updates to return a promise

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 var pug = require("pug");
 var phantom = require("phantom");
+var Promise = require("promise");
 
-(function () {
+// String -> Promise
+function pugToImage(pugString) {
     let page, status, instance, base64;
     const address = "http://github.com";
 
-    phantom.create().then(function (_instance) {
-        instance = _instance;
+    return new Promise(function (resolve, reject) {
+        phantom.create().then(function (_instance) {
+            instance = _instance;
 
-        return instance.createPage();
-    }).then(function (_page) {
-        page = _page;
-        page.property("viewportSize", { width: 600, height: 600 });
+            return instance.createPage();
+        }).then(function (_page) {
+            page = _page;
+            page.property("viewportSize", { width: 600, height: 600 });
 
-        return page.open(address);
-    }).then(function (_status) {
-        status = _status;
+            return page.open(address);
+        }).then(function (_status) {
+            status = _status;
 
-        return page.renderBase64("PNG");
-    }).then(function (_base64) {
-        base64 = _base64;
+            return page.renderBase64("PNG");
+        }).then(function (_base64) {
+            base64 = _base64;
 
-        console.log(Buffer.from(base64, "base64"));
+            resolve(Buffer.from(base64, "base64"));
 
-        phantom.exit(1);
+            phantom.exit(1);
+        }).catch(function (error) {
+            reject(error);
+        });
     });
-}());
-
-// String -> Buffer
-function pugToImage(pugString) {
-    
 }
 
 module.exports = pugToImage;
